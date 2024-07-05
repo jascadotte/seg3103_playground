05dbb87ab36bda633de7dc6339aeb6cd0132af79
jascadotte <jascadotte@gmail.com>
2024-07-04 22:50:20 -0400

    refactor 1

--- assignment2/grades/lib/grades/calculator.ex
+++ assignment2/grades/lib/grades/calculator.ex
@@ -1,37 +1,23 @@
 defmodule Grades.Calculator do
-  def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
+  def avg(grades) do
+    if Enum.count(grades) == 0 do
+      0
+    else
+      Enum.sum(grades) / Enum.count(grades)
+    end
+  end
 
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+  def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
 
     mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
     round(mark * 100)
   end
 
   def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
-
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
 
     avg_exams = (midterm + final) / 2
 
@@ -62,19 +48,8 @@ defmodule Grades.Calculator do
   end
 
   def numeric_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
-
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
 
     avg_exams = (midterm + final) / 2
 
--- assignment2/grades/test/grades/calculator_test.exs
+++ assignment2/grades/test/grades/calculator_test.exs
@@ -2,15 +2,49 @@ defmodule Grades.CalculatorTest do
   use ExUnit.Case
   alias Grades.Calculator
 
-  describe "percentage_grade/1" do
-    test "sample" do
-      assert 85 ==
-               Calculator.percentage_grade(%{
-                 homework: [0.8],
-                 labs: [1, 1, 1],
-                 midterm: 0.70,
-                 final: 0.9
-               })
+  describe "percentage_grade" do
+    test "Fail" do
+      assert 0 == Calculator.percentage_grade(%{homework: [], labs: [], midterm: 0, final: 0 })
+    end
+
+    test "Pass" do
+      assert 50 == Calculator.percentage_grade(%{homework: [0.5], labs: [0.5], midterm: 0.5, final: 0.5 })
+    end
+  end
+
+  describe "letter_grade" do
+    # Cycle all standard grades
+    test "A+ to E" do
+      for {grade, score} <- Map.to_list(%{"A+" => 1, "A" => 0.85, "A-" => 0.8, "B+" => 0.75, "B" => 0.7, "C+" => 0.65, "C" => 0.6, "D" => 0.5, "E" => 0.45 }) do
+          assert Calculator.letter_grade(%{homework: [score], labs: [score, score, score], midterm: score, final: score}) == grade
+      end
+    end
+
+    test "F" do
+      # Labs at least .25 Homework and Final at least .4
+      assert "F" == Calculator.letter_grade(%{homework: [0.4], labs: [0.25,0.25,0.25], midterm: 0.4, final: 0.4 })
+    end
+
+    test "EIN" do
+      assert "EIN" == Calculator.letter_grade(%{homework: [], labs: [], midterm: 0, final: 0 })
+    end
+  end
+
+
+  describe "numeric_grade" do
+    test "10 to 1" do
+      for {grade, score} <- Map.to_list(%{10 => 1, 9 => 0.85, 8 => 0.8, 7 => 0.75, 6 => 0.7, 5 => 0.65, 4 => 0.6, 3 => 0.55, 2 => 0.5, 1 => 0.45 }) do
+          assert Calculator.numeric_grade(%{homework: [score], labs: [score, score, score], midterm: score, final: score}) == grade
+      end
+    end
+
+    test "00" do
+      #Labs at least .25 Homework and Final at least .4
+      assert 0 == Calculator.numeric_grade(%{homework: [0.4], labs: [0.25,0.25,0.25], midterm: 0.45, final: 0.45 })
+    end
+
+    test "EIN" do
+      assert 0 == Calculator.numeric_grade(%{homework: [], labs: [], midterm: 0, final: 0 })
     end
   end
 end
